637b482aabf4d03b0e5dce8a2b17d279e6e7157e
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-11-05 09:36:13 +0800

    2025年11月5日

--- 2-物种树构建/script/2-4-VeryFastTree.sh
+++ 2-物种树构建/script/2-4-VeryFastTree.sh
@@ -13,4 +13,6 @@ $SOFTWARE \
     "$OUTPUT_TREE"
 
 
-python3 /home/luolintao/test_mail.py "2-物种树构建/script/2-4-VeryFastTree.sh任务完成通知" "<p>2-物种树构建/script/2-4-VeryFastTree.sh分析已完成，请查看结果目录。</p>"
+python3 /home/luolintao/test_mail.py \
+    "2-物种树构建/script/2-4-VeryFastTree.sh任务完成通知"\
+    "<p>2-物种树构建/script/2-4-VeryFastTree.sh分析已完成，请查看结果目录。</p>"
